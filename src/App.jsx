137cc421eea1338ec209ae101f9b0436f91ba87a
Jason C. Liu <32473162+TropicalSunshine@users.noreply.github.com>
2020-07-25 22:38:34 -0700

    delete app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import React, {Component} from 'react';
 
 
-
-
-import './App.css';
-
 import Router from "./shared/utils/Router";
 import ErrorBoudary from "./shared/utils/ErrorBoundary";
 
 export default class App extends Component{
-  constructor(){
-    super();
-  }
-
-
-
   render(){
-
-    
     return (
       <ErrorBoudary>
         <Router/>
