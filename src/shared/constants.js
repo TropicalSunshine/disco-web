d2781eed0cc8a035788814937102ba6cb5d3cf6b
Jason C. Liu <32473162+TropicalSunshine@users.noreply.github.com>
2020-08-26 14:48:21 -0700

    replace constants with .env variables

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,6 +1,6 @@
 const socketProtocol    = (window.location.protocol.includes('https')) ? 'wss' : 'ws';
 export const socketUrl  = `${socketProtocol}://${process.env.REACT_APP_SOCKET_URL}`;
 
-export const baseApiUrl         = "https://disco-gateway.herokuapp.com";
-export const graphqlEndpoint    = "/api/v1/graphql";
+export const baseApiUrl         = process.env.REACT_APP_API_URL;
+export const graphqlEndpoint    = process.env.REACT_APP_API_ENDPOINT;
 export const apiUrl             = `${baseApiUrl}${graphqlEndpoint}`;
