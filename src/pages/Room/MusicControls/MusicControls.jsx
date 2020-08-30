267a8e3be8c7be8070e92b05a289285eca22c942
Jason C. Liu <32473162+TropicalSunshine@users.noreply.github.com>
2020-08-30 15:50:01 -0700

    try prettier code formatting

--- src/pages/Room/MusicControls/MusicControls.jsx
+++ src/pages/Room/MusicControls/MusicControls.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
 import styles from "./styles.module.css";
 
-import PlayIcon from "@material-ui/icons/PlayArrowSharp"
+import PlayIcon from "@material-ui/icons/PlayArrowSharp";
 import PauseIcon from "@material-ui/icons/PauseSharp";
 import FastForwardIcon from "@material-ui/icons/FastForwardSharp";
 import FastRewindIcon from "@material-ui/icons/FastRewindSharp";
-
 
 /*
 
@@ -16,65 +15,45 @@ props req
     handlePrev
  */
 
-function MusicControls(props){
+function MusicControls(props) {
+  const { musicRoom } = props;
 
-    const { musicRoom } = props;
+  const { paused, pause, play } = musicRoom;
 
-    const {
-        paused,
-        pause,
-        play
-    } = musicRoom;
-
-
-    const handlePausePlay = () => {
-        if(paused) {
-            console.log("playing");
-            play();
-        } else {
-            console.log("pausing");
-            pause();
-        } 
+  const handlePausePlay = () => {
+    if (paused) {
+      console.log("playing");
+      play();
+    } else {
+      console.log("pausing");
+      pause();
     }
+  };
 
-    return (
-        <div className={styles["music-player-container"]}>
-            <div className="box-center">
-                <div className={`${styles["music-player-control-button"]} ${styles["rewind-button"]}`}>
-                    <FastRewindIcon
-                    className={`${styles.icon}`}
-                    />
-                </div>
-                <div 
-                className={`${styles["music-player-control-button"]} 
-                ${styles["play-button"]}`}
-                onClick={handlePausePlay}
-                >
-                    {
-                        (paused) &&
-                        (
-                            <PlayIcon 
-                            className={`${styles.icon}`}
-                            />
-                        )
-                    }
-                    {
-                        (!paused) && (
-                            <PauseIcon
-                            className={`${styles.icon}`}
-                            />
-                        )
-                    }
-                </div>
-                <div className={`${styles["music-player-control-button"]} ${styles["rewind-button"]}`}>
-                    <FastForwardIcon
-                    className={`${styles.icon}`}
-                    />
-                </div>
-            </div>
+  return (
+    <div className={styles["music-player-container"]}>
+      <div className="box-center">
+        <div
+          className={`${styles["music-player-control-button"]} ${styles["rewind-button"]}`}
+        >
+          <FastRewindIcon className={`${styles.icon}`} />
         </div>
-    )
+        <div
+          className={`${styles["music-player-control-button"]} 
+                ${styles["play-button"]}`}
+          onClick={handlePausePlay}
+        >
+          {paused && <PlayIcon className={`${styles.icon}`} />}
+          {!paused && <PauseIcon className={`${styles.icon}`} />}
+        </div>
+        <div
+          className={`${styles["music-player-control-button"]} ${styles["rewind-button"]}`}
+        >
+          <FastForwardIcon className={`${styles.icon}`} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MusicControls;
-
--- src/pages/Room/Room.jsx
+++ src/pages/Room/Room.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 import { LoaderPage } from "shared/components/index";
 
@@ -6,85 +6,69 @@ import MessagePanel from "./MessagePanel";
 import MusicControls from "./MusicControls";
 import SearchPanel from "./SearchPanel";
 
-
-
 import { useParams } from "react-router-dom";
 
-import styles from "./style.module.css"
+import styles from "./style.module.css";
 
+function Room(props) {
+  const { roomId } = useParams();
+  const { musicRoom } = props;
 
-function Room(props){
+  const [isLoading, setIsLoading] = useState(true);
 
-    const { roomId } = useParams();
-    const { musicRoom } = props;
+  const { paused, songImage, join, leave } = musicRoom;
 
-    const [ isLoading, setIsLoading ] = useState(true);
+  const [loadingValue, setLoadingValue] = useState(0);
 
-    const {
-        paused,
-        songImage,
-        join,
-        leave
-    } = musicRoom;
-    
-    const [ loadingValue, setLoadingValue ] = useState(0);
+  /* eslint-disable */
+  useEffect(() => {
+    console.log(musicRoom);
 
-    /* eslint-disable */
-    useEffect(() => {
-        console.log(musicRoom);
+    (async () => {
+      setIsLoading(true);
+      await join(roomId);
+      setLoadingValue(100);
 
-        (async () => {
-            setIsLoading(true);
-            await join(roomId);
-            setLoadingValue(100);
-            
-            setIsLoading(false);
-        })();
+      setIsLoading(false);
+    })();
 
-        return () => {
-            leave();
-        }
+    return () => {
+      leave();
+    };
+  }, []);
+  /* eslint-enable */
 
-    }, []);
-    /* eslint-enable */
-
-    return (
-        <React.Fragment>
-            {
-                (isLoading) && (
-                    <LoaderPage value={loadingValue}/>
-                )
-            }
-            {
-                (!isLoading) && (
-                    <div className={`box-row ${styles["room"]}`}>
-                        <div className={styles["room__room-left"]}>
-                            <SearchPanel/>
-                        </div>
-                        <div className={`box-column ${styles["room__room-central"]}`}>
-                            
-                            <div className={`box-center ${styles["room__player-display-box"]}`}>
-                                <div className={styles["room__player-disk"]} style={{
-                                    animationPlayState : ((!paused) ? "running" : "paused"),
-                                    backgroundImage : `url(${songImage})`
-                                }}>
-                                    <i/>
-                                </div>
-                            </div>
-                            <div className={styles["player-control-container"]}>
-                                <MusicControls/>
-                            </div>
-                        
-                        </div>
-                        <div className={styles["room__room-right"]}>
-                                <MessagePanel/>
-                        </div>
-                        
-                    </div>
-                )
-            }
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      {isLoading && <LoaderPage value={loadingValue} />}
+      {!isLoading && (
+        <div className={`box-row ${styles["room"]}`}>
+          <div className={styles["room__room-left"]}>
+            <SearchPanel />
+          </div>
+          <div className={`box-column ${styles["room__room-central"]}`}>
+            <div className={`box-center ${styles["room__player-display-box"]}`}>
+              <div
+                className={styles["room__player-disk"]}
+                style={{
+                  animationPlayState: !paused ? "running" : "paused",
+                  backgroundImage: `url(${songImage})`,
+                }}
+              >
+                <i />
+              </div>
+            </div>
+            <div className={styles["player-control-container"]}>
+              <MusicControls />
+            </div>
+          </div>
+          <div className={styles["room__room-right"]}>
+            <MessagePanel />
+          </div>
+        </div>
+      )}
+    </React.Fragment>
+  );
 }
 
 export default Room;
