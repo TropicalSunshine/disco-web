c622d0139cbbe758f6a08a69883842c7e0fdd6c4
Jason C. Liu <32473162+TropicalSunshine@users.noreply.github.com>
2020-08-31 11:40:03 -0700

    add react-app-rewired, config webpack aliases

---
+++ config-overrides.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = function override(config, env){
+
+    var { alias } = config.resolve;
+
+    config.resolve.alias = {
+        ...alias,
+        shared : path.resolve(__dirname, "./src/shared"),
+        assets : path.resolve(__dirname, "./src/assets"),
+        css : path.resolve(__dirname, "./src/css"),
+        pages : path.resolve(__dirname, "./src/pages")
+    }
+
+    return config;
+}
