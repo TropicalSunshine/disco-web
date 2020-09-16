c056cc1a72e98e44b084c614a982dd4fe058741e
Jason C. Liu <32473162+TropicalSunshine@users.noreply.github.com>
2020-09-16 11:09:06 -0700

    center spinner in loader page/ refactor code

--- src/shared/components/index.js
+++ src/shared/components/index.js
@@ -2,7 +2,7 @@ import InputSubmit from "./form/InputSubmit";
 
 import IconButton from "./control/IconButton";
 
-import LoaderPage from "./loaders/LoaderPage/LoaderPage.jsx"; 
+import LoaderPage from "./loaders/LoaderPage"; 
 import Spinner from "./loaders/Spinner";
 
 import TextArea from "./control/TextArea";
--- src/shared/components/loaders/LoaderPage/LoaderPage.jsx
+++ src/shared/components/loaders/LoaderPage/LoaderPage.jsx
@@ -1,58 +1,16 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import CircularProgress from "@material-ui/core/CircularProgress"; 
-import "./LoaderPage.css";
+import styles from "./LoaderPage.module.css";
 
-export default class LoaderPage extends PureComponent {
-    constructor(props){
-
-        super(props);
-
-        this.interval = null;
-        this.state = {
-            message: "",
-            messageIndex: 0
-        }
-    }
-
-    componentDidMount(){
-        /*
-        this.interval = setInterval(() => {
-            var { messageIndex } = this.state;
-
-            this.setState({
-                message: this.messages[messageIndex]
-            })
-            
-            messageIndex++;
-
-            if(messageIndex === this.messages.length){
-                messageIndex = 0;
-            }
-
-            this.setState({
-                messageIndex : messageIndex
-            });
-            
-
-        }, 1500);
-        */
-    }
-
-    componentWillUnmount(){
-        clearInterval(this.interval);
-    }
-
-
-    
-    render() {
-        return (
-            <div className="loaderpage-container">
-                <div className="loaderpage-center-box">
-                    <div className="box-center">
-                        <CircularProgress/>
-                    </div>
-                </div>
+function LoaderPage() {
+    return (
+        <div className={styles["loaderpage-container"]}>
+            <div className={styles["loaderpage-center-box box-center"]}>            
+                <CircularProgress/>
             </div>
-        )
-    }
+        </div>
+    )
+    
 }
+
+export default LoaderPage;
