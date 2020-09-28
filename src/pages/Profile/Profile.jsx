5d813de2863d91e0777ebd67c0c6994cec399703
TropicalSunshine <jasonliu21396@gmail.com>
2020-09-28 16:29:26 -0700

    add userApi for getting user by username

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,20 +1,62 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Spinner, UserProfileIcon } from "shared/components";
+
+import { useUser } from "shared/context/user";
+import { User as UserApi } from "shared/utils/api";
+
+import { textStyles } from "shared/styles";
+import styles from "./Profile.module.css";
 
 /* eslint-disable */
-const CURRENT_USER_SLUG = "@me";
 
 function Profile(props) {
 
+    const { user } = useUser();
     const { username } = useParams();
 
-    useEffect(() => {
+    const [userProfile, setUserProfile] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
-    }, []);
+    const loadProfile = async () => {
+        const response = await UserApi.getUserByUsername(username);
+        setUserProfile(response.data.data.getUserByUsername);
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        if (!user) return;
+        if (username === user.username) {
+            setUserProfile(user);
+        } else {
+            loadProfile();
+        }
+
+    }, [user]);
 
     return (
-        <div>
-
+        <div className={`box-center ${styles["profile"]}`}>
+            { (isLoading) && <Spinner />}
+            {
+                (!isLoading) && (
+                    <div className="box-column">
+                        <div>
+                            <UserProfileIcon
+                                height="200px"
+                                label={userProfile.username}
+                            />
+                        </div>
+                        <div>
+                            <h1 className={`
+                                ${styles["profile__text__username"]}
+                                ${textStyles["text-main"]}
+                            `}>
+                                {userProfile.username}
+                            </h1>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
