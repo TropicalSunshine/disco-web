0acac5f198fdaf2a98482d38a3020933681f4450
Jason C. Liu <32473162+TropicalSunshine@users.noreply.github.com>
2020-10-07 11:21:46 -0700

    clean up debugging statements

--- src/shared/context/auth/index.js
+++ src/shared/context/auth/index.js
@@ -1,4 +1,4 @@
-import withAuthProvider from "./withAuthProvider.jsx";
+import withAuthProvider from "./withAuthProvider";
 import withAuthCheck from "./withAuthCheck";
 import AuthContext from "./AuthContext";
 import useAuth from "./useAuth";
--- src/shared/context/auth/withAuthProvider.jsx
+++ src/shared/context/auth/withAuthProvider.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AuthContext from "./AuthContext.js";
+import AuthContext from "./AuthContext";
 
 const withAuthProvider = Component => class extends React.Component {
 
