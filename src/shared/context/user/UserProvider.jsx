6967671d441bcb3069c1a0c89799b4f81ccaa0d6
Jason C. Liu <32473162+TropicalSunshine@users.noreply.github.com>
2020-12-23 20:20:17 -0800

    display loading page while requesting cached user info

--- src/shared/context/user/UserProvider.jsx
+++ src/shared/context/user/UserProvider.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 
+import { LoaderPage } from "shared/components";
+
 import UserContext from "./UserContext";
 import useUserProvider from "./useUserProvider";
+
 
 function UserProvider({ children }) {
 
     const userProvider = useUserProvider();
-
+    
     return (
         <UserContext.Provider value={{
             ...userProvider
         }}>
-            {children}
+            {
+                (!userProvider.isUserLoaded) && <LoaderPage/>
+            }
+            {
+                (userProvider.isUserLoaded) && (children)
+            }
         </UserContext.Provider>
     )
 }
--- src/shared/context/user/useUserProvider.js
+++ src/shared/context/user/useUserProvider.js
@@ -6,22 +6,28 @@ function useUserProvider() {
 
     const { isLoggedIn } = useAuth();
     const [user, setUser] = useState({});
+    const [isUserLoaded, setIsUserLoaded] = useState(false)
 
     const getUser = async () => {
         const response = await UserApi.me();
         const userProfile = response.data.data.me;
+        setIsUserLoaded(true);
 
         setUser(userProfile);
     }
 
     useEffect(() => {
         if (isLoggedIn) getUser();
+        else {
+            setIsUserLoaded(true);
+        }
         return;
     }, [isLoggedIn])
 
     return {
         user,
-        getUser
+        getUser,
+        isUserLoaded
     }
 }
 
